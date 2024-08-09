db93e6a05741547d3681ed055bdc28dbfe8877dc
HemanthKumar.Pasham1 <HemanthKumar.Pasham@rockwellautomation.com>
2024-08-09 15:28:44 +0530

    First commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from redis import from_url
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +7,12 @@ app = FastAPI()
 @app.get("/get")
 def get_service():
     return {"status":"success", "message":"Hey ! your first project is built on docker"}
+
+@app.get("/redis")
+def insert_redis_data():
+    redis_client = from_url(url="redis://13.60.98.95:6379", db=0)
+    redis_client.set("hemanth1", "enjoy pandagoooo1234")
+    return {"status":"success"}
 
 
 uvicorn.run(app, host="0.0.0.0", port=9090)
